4eda77b1beed352ba88aa223a0645e360d5eb9fd
sineth <sinethneranjana@gmail.com>
2017-08-27 10:20:00 +0530

    misc

---
+++ app/src/main/java/lk/ac/mrt/uom/uomonline/ui/SplashScreen.java
@@ -0,0 +1,58 @@
+package lk.ac.mrt.uom.uomonline.ui;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.chootdev.csnackbar.Duration;
+import com.chootdev.csnackbar.Snackbar;
+import com.chootdev.csnackbar.Type;
+
+import lk.ac.mrt.uom.uomonline.MainActivity;
+import lk.ac.mrt.uom.uomonline.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+
+        Snackbar.with(this, null)
+                .type(Type.ERROR)
+                .message("No Internet Connection Available")
+                .duration(Duration.INFINITE)
+                .show();
+        runAnimation();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                overridePendingTransition(R.anim.activity_fade_in, R.anim.activity_fade_out);
+                startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    private void runAnimation() {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        a.reset();
+        TextView tv = (TextView) findViewById(R.id.splashText);
+        tv.clearAnimation();
+        tv.startAnimation(a);
+    }
+}
